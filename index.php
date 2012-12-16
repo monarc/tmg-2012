f176017ca30571bf8a0956aa362df3c55de0ee84
Bryan <bryan@tmgamer.co>
2012-12-16 11:49:23 +0800

    Added more images Modified index.php

--- index.php
+++ index.php
@@ -3,28 +3,27 @@
 <link href='css/main.css' type="text/css" rel="stylesheet"/>
 <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script> 
 <title>3DS Monster</title>  
- 
+
 </head>
 
 <body>
 
 </head>
 <body> 
-	
-	<?php include "header.php"; ?>
-    
-	<div class="tic_body" align="center">
-		<b>Welcome to 3DS Monsters</b>
-		<br><br><br>
-		<a href="tutorial1.php">
-			<span class="tic_button"> Click to tutorials</span>
-		</a>
-		<br><br>
-	</div>
-	 
-	
-	<?php include "footer.php"; ?>
-	
+    <div class="tic_smartphone">
+        <?php include "header.php"; ?>
+
+        <div class="tic_body" align="center">
+            <div style="padding:5px">
+                <img src="images/game/Game_image_07.png" />
+            </div>
+            <br />
+            <a href="#"><div style="border-radius: 5px; border: 1px solid #A9A9A9; padding:5px; width:100px; margin:5px;">BEGIN</div></a>
+        </div>
+
+
+        <?php include "footer.php"; ?>
+    </div>
 </body>
 </html>
 
